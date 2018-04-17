e1c5bf1d7fa0da0e623028481998b5c8c86a237c
piyushvarshney <piyushvarshney@outlook.com>
2018-04-17 03:38:31 +0000

    Added main py file

---
+++ demo.py
@@ -0,0 +1,27 @@
+#Author: Piyush Varshney <http://www.piyushvarshney.cf> Github: https://github.com/piyushvarshney
+import tweepy
+from textblob import TextBlob
+
+# Step 1 - Authenticate
+consumer_key= 'CONSUMER_KEY_HERE'
+consumer_secret= 'CONSUMER_SECRET_HERE'
+
+access_token='ACCESS_TOKEN_HERE'
+access_token_secret='ACCESS_TOKEN_SECRET_HERE'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 2 - Retrieve Tweets
+public_tweets = api.search('Trump')
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+    
+    #Step 3 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print("")
